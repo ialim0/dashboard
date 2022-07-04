444634fbd9c569a2aed9e37e345d1b1da48654a0
ialim0 <i.alim0229@gmail.com>
2022-07-04 17:31:06 +0000

    chart building

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,7 +1,10 @@
 import 'package:admin/constraints.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+import 'components/chart.dart';
 import 'components/header.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -13,7 +16,52 @@ class DashboardScreen extends StatelessWidget {
       child: SingleChildScrollView(
         padding: EdgeInsets.all(defaultPadding),
         child: Column(
-          children: [Header()],
+          children: [
+            Header(),
+            SizedBox(
+              height: defaultPadding,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    height: 500,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: defaultPadding,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    height: 500,
+                    decoration: BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            "Storage details",
+                            style: GoogleFonts.adamina(
+                                fontSize: 21, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Chart()
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
